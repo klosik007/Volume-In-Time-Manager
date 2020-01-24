9c349bcedf1896339a200138a4e7c2b3562f4a53
Przemyslaw Klos <pklos1992@gmail.com>
2020-01-24 21:33:55 +0100

    linear layouts displaying from JSON

--- app/src/main/java/com/example/volumeintimemanager/MainActivity.java
+++ app/src/main/java/com/example/volumeintimemanager/MainActivity.java
@@ -9,10 +9,21 @@ import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    RelativeLayout layout;
     public static final String EXTRA_MESSAGE = "com.example.volumeintimemanager.MESSAGE";
 
     public void addRule(View view){
@@ -25,13 +36,58 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    private void displayRules(String json){
-        TextView timeFrom;
-        TextView timeTo;
-        TextView daysOfWeek;
-        CheckBox isRuleInUse;
+    private List<List<String>> loadRulesFromJSON(String json){
+        try{
+            JSONObject previousJSONObj = new JSONObject(json);
+            JSONArray obj = previousJSONObj.getJSONArray("rules");
+            int objLength = obj.length();
 
+            List<List<String>> listOfLists = new ArrayList<>();
+            for (int i = 0; i < objLength; i++){
+                JSONObject rule = obj.getJSONObject(i);
+                List<String> jsonList = new ArrayList<>();
+                jsonList.add(Integer.toString(rule.optInt("id")));
+                jsonList.add(rule.optString("timeFrom"));
+                jsonList.add(rule.optString("timeTo"));
+                jsonList.add(rule.optString("daysOfWeek"));
+                jsonList.add(Integer.toString(rule.optInt("spinnerChoice")));
+                jsonList.add(Boolean.toString((rule.optBoolean("isRuleInUse"))));
+                listOfLists.add(jsonList);
+            }
 
+            return listOfLists;
+        }catch (JSONException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private void displayRules(List<List<String>> rules){
+        if (!rules.isEmpty()){
+            layout = (RelativeLayout)findViewById(R.id.relativeLayout);
+
+            UserInterfaceHandler userInterface = new UserInterfaceHandler(MainActivity.this);
+            List<LinearLayout> layoutsList = new ArrayList<>();
+
+            for (List<String> rl : rules){
+                layoutsList.add(userInterface.createLinearLayoutForRule(Integer.parseInt(rl.get(0)), Boolean.parseBoolean(rl.get(5)), rl.get(1), rl.get(2), rl.get(3), Integer.parseInt(rl.get(4))));
+            }
+
+            List<RelativeLayout.LayoutParams> params = new ArrayList<>();
+            int rulesListSize = rules.size();
+            for (int i = 0; i < rulesListSize; i++){
+                params.add(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+            }
+
+            for (int i = 1; i < params.size(); i++){
+                params.get(i).addRule(RelativeLayout.BELOW, layoutsList.get(i-1).getId());
+            }
+
+            int layoutsListSize = layoutsList.size();
+            for (int i = 0; i < layoutsListSize; i++){
+                layout.addView(layoutsList.get(i), params.get(i));
+            }
+        }
     }
 
     @Override
@@ -40,19 +96,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //file with rules reference
-        FilesHandler fileHandler = new FilesHandler();
+        FilesHandler fileHandler = new FilesHandler(MainActivity.this);
         //managing JSONs reference
-        JSONFilesHandler JSONHandler = new JSONFilesHandler();
+        //JSONFilesHandler JSONHandler = new JSONFilesHandler(MainActivity.this);
 
-        boolean isFileAvailable = fileHandler.isFileAvailable(MainActivity.this, "rules.json");
+        boolean isFileAvailable = fileHandler.isFileAvailable("rules.json");
         if (isFileAvailable){
-            String jsonString = fileHandler.readFile(MainActivity.this, "rules.json");
+            String jsonString = fileHandler.readFile("rules.json");
             Log.d("MainActivity", jsonString);
             //read rules from file and display them in main window on start
-            //displayRules(jsonString);
+            List<List<String>> rls = loadRulesFromJSON(jsonString);
+            displayRules(rls);
         } else{
             //first launch of app - create empty rules.json file
-            boolean isFileCreated = fileHandler.createFile(MainActivity.this, "rules.json", "{\"rules\":[]}");
+            boolean isFileCreated = fileHandler.createFile("rules.json", "{\"rules\":[]}");
             if (isFileCreated){
                 Toast.makeText(MainActivity.this, "File rules.json created", Toast.LENGTH_LONG).show();
             } else{
@@ -60,4 +117,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
+    //onActivityResult()
 }

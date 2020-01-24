076c4945b4b620a9cf0e802de45cb5c34336e1e1
Przemyslaw Klos <pklos1992@gmail.com>
2020-01-24 21:30:43 +0100

    added spinner handler, added more info to be saved

--- app/src/main/java/com/example/volumeintimemanager/AddRule.java
+++ app/src/main/java/com/example/volumeintimemanager/AddRule.java
@@ -10,12 +10,20 @@ import android.os.Bundle;
 import android.text.InputType;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TimePicker;
 
 import java.io.File;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.List;
 
 public class AddRule extends AppCompatActivity {
     TimePickerDialog timePicker;
@@ -24,6 +32,7 @@ public class AddRule extends AppCompatActivity {
     EditText dayPickerEditText;
     Button addRuleButton;
     String daysPicked;
+    int behaviorSpinnerChoice;
 
     public final static String MESSAGE_KEY = "com.example.volumeintimemanager.message_key";
 
@@ -49,10 +58,6 @@ public class AddRule extends AppCompatActivity {
         }
     };
 
-//    private void displayRules(String file){
-//
-//    }
-
     private String checkDigit(int number){
         return number <= 9 ? "0"+ number : String.valueOf(number);
     }
@@ -62,20 +67,84 @@ public class AddRule extends AppCompatActivity {
         startActivityForResult(intent, 1);
     }
 
+    public String replaceDaysIDsWithDaysNames(String daysIds){
+        if (daysIds == null){
+            throw new Error("Days picked must not be null");
+        }
+
+        Dictionary days = new Hashtable();
+        days.put("1", "Sunday");
+        days.put("2", "Monday");
+        days.put("3", "Tuesday");
+        days.put("4", "Wednesday");
+        days.put("5", "Thursday");
+        days.put("6", "Friday");
+        days.put("7", "Saturday");
+
+        String[] daysIDs = daysIds.split(";");
+        List<String> daysNames = new ArrayList<>();
+
+        for (String day : daysIDs){
+            daysNames.add((String) days.get(day));
+        }
+
+        int daysNamesSize = daysNames.size();
+        String daysOutput = "";
+        for (int i = 0; i < daysNamesSize; i++){
+            daysOutput += daysNames.get(i) + ";";
+        }
+
+        return daysOutput;
+    }
+
     public void addRuleButton(View view){
         //file with rules reference
-        JSONFilesHandler json = new JSONFilesHandler();
-        String jsonString = json.readFile(AddRule.this, "rules.json");
+        JSONFilesHandler json = new JSONFilesHandler(AddRule.this);
+        String jsonString = json.readFile("rules.json");
         Integer id = json.returnLastIdFromJSON(jsonString) + 1;
         String timeFromText = timeFromEditText.getText().toString();
         String timeToText = timeToEditText.getText().toString();
-        String daysPickedText =  dayPickerEditText.getText().toString();
+        String daysPickedText = daysPicked;
+        int spinnerChoice = behaviorSpinnerChoice;
 
-        String rule = json.addRuleToJSON(AddRule.this, "rules.json", id, timeFromText, timeToText, daysPickedText, true);
-        json.overwriteJSONFile(AddRule.this, "rules.json", rule);
+        String rule = json.addRuleToJSON("rules.json", id, timeFromText, timeToText, daysPickedText, spinnerChoice,true);
+        json.overwriteJSONFile("rules.json", rule);
 
-        String jsonString2 = json.readFile(AddRule.this, "rules.json");
+        String jsonString2 = json.readFile("rules.json");
         Log.d("AddRule", jsonString2);
+    }
+
+    private void loadSpinnerItems(){
+        Spinner behaviorSpinner = (Spinner)findViewById(R.id.behaviorSpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.behaviorSpinner_array, android.R.layout.simple_spinner_item);
+        //which layout to use when list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //apply adapter to a spinner
+        behaviorSpinner.setAdapter(adapter);
+        //set select listener to a spinner
+        behaviorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Long positionIdLong = parent.getItemIdAtPosition(position);
+                Integer positionIdInt = positionIdLong != null ? positionIdLong.intValue() : null; //suitable for API >= 19
+                //Integer positionIdInt = positionIdLong == null ? null : Math.toIntExact(positionIdLong);  //min API 24
+                switch (positionIdInt) {
+                    case 0:
+                        behaviorSpinnerChoice = 0; // SOUNDS OFF
+                        break;
+                    case 1:
+                        behaviorSpinnerChoice = 1; //SOUNDS ON
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // TODO Auto-generated method stub
+            }
+        });
     }
 
     @Override
@@ -97,6 +166,8 @@ public class AddRule extends AppCompatActivity {
         //set onClick event for text boxes
         timeFromEditText.setOnClickListener(showTimePicker);
         timeToEditText.setOnClickListener(showTimePicker);
+
+        loadSpinnerItems();
     }
 
     @Override
@@ -105,7 +176,8 @@ public class AddRule extends AppCompatActivity {
         if (requestCode == 1) {
             if(resultCode == Activity.RESULT_OK){
                 daysPicked = data.getStringExtra(MESSAGE_KEY);
-                dayPickerEditText.setText(daysPicked);
+                String daysPickedNames = replaceDaysIDsWithDaysNames(daysPicked);
+                dayPickerEditText.setText(daysPickedNames);
             }
             if (resultCode == Activity.RESULT_CANCELED) {
                 //Write your code if there's no result

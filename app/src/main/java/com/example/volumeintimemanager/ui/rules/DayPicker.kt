636d490b3f1049a4e82e24448a8847bc15bf8f4f
Przemysław Kłos <pklos1992@gmail.com>
2023-10-26 16:56:05 +0200

    DayPicker - react on day canvas click (fill the circle, change text color)

--- app/src/main/java/com/example/volumeintimemanager/ui/rules/DayPicker.kt
+++ app/src/main/java/com/example/volumeintimemanager/ui/rules/DayPicker.kt
@@ -1,18 +1,15 @@
 package com.example.volumeintimemanager.ui.rules
 
-import android.graphics.Paint
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.material.Divider
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.getValue
@@ -22,25 +19,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.DrawStyle
+import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.volumeintimemanager.sampledata.RulesRepo
-import com.example.volumeintimemanager.ui.home.dashboard.RuleRow
 
 @Composable
 fun DayPicker() {
     val weekDaysLetters by remember { mutableStateOf(listOf("M", "T", "W", "T", "F", "S", "S")) }
 
-    MaterialTheme() {
-        Surface {
-            Row() {
-                weekDaysLetters.forEach {
-                    Column(modifier = Modifier.padding(5.dp)) {
-                        DayCircle(weekDay = it)
-                    }
-                }
+    Row() {
+        weekDaysLetters.forEach {
+            Column(modifier = Modifier.padding(5.dp)) {
+                DayCircle(weekDay = it)
             }
         }
     }
@@ -48,9 +39,15 @@ fun DayPicker() {
 
 @Composable
 private fun DayCircle(weekDay: String) {
+    val isFilled = remember { mutableStateOf(false) }
+
     Box {
         Canvas(
-            modifier = Modifier.size(40.dp),
+            modifier = Modifier
+                .size(40.dp)
+                .clickable {
+                    isFilled.value = !isFilled.value
+                },
             onDraw = {
                 val canvasWidth = size.width
                 val canvasHeight = size.height
@@ -58,12 +55,14 @@ private fun DayCircle(weekDay: String) {
                     color = Color.Blue,
                     center = Offset(x = canvasWidth / 2, y = canvasHeight / 2),
                     radius = 40f,
-                    style = Stroke(10F))
+                    style = if (isFilled.value) Fill else Stroke(5f)
+                )
             }
         )
         Text(
             modifier = Modifier.align(Alignment.Center),
-            text = weekDay, color = Color.Black,
+            text = weekDay,
+            color = if (isFilled.value) Color.White else Color.Black,
             fontSize = 16.sp
         )
     }
@@ -72,11 +71,15 @@ private fun DayCircle(weekDay: String) {
 @Preview
 @Composable
 private fun DayCirclePreview() {
-    DayCircle("M")
+    Surface {
+        DayCircle("M")
+    }
 }
 
 @Preview
 @Composable
 private fun DayPickerPreview() {
-    DayPicker()
+    Surface {
+        DayPicker()
+    }
 }

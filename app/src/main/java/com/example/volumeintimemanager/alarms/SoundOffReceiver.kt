490d0804c27c95cfd2e008aad56e55ae06e88f6b
Przemysław Kłos <pklos1992@gmail.com>
2023-11-24 15:46:02 +0100

    Broadcast receivers for turning sounds on/off

---
+++ app/src/main/java/com/example/volumeintimemanager/alarms/SoundOffReceiver.kt
@@ -0,0 +1,13 @@
+package com.example.volumeintimemanager.alarms
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.media.AudioManager
+
+class SoundOffReceiver(): BroadcastReceiver() {
+    override fun onReceive(p0: Context?, p1: Intent?) {
+        val audioManager = p0?.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        audioManager.ringerMode = AudioManager.RINGER_MODE_SILENT
+    }
+}

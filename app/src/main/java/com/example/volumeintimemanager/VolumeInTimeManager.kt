73df5c204ac8b33f3534ff3f07dfc77a164ec449
Przemysław Kłos <pklos1992@gmail.com>
2023-11-27 14:53:25 +0100

    AlarmSchedulerImpl instance in app state

--- app/src/main/java/com/example/volumeintimemanager/VolumeInTimeManager.kt
+++ app/src/main/java/com/example/volumeintimemanager/VolumeInTimeManager.kt
@@ -1,7 +1,16 @@
 package com.example.volumeintimemanager
 
 import android.app.Application
+import com.example.volumeintimemanager.alarms.AlarmSchedulerImpl
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class VolumeInTimeManager: Application()
+class VolumeInTimeManager: Application() {
+
+    lateinit var alarmScheduler: AlarmSchedulerImpl
+
+    override fun onCreate() {
+        super.onCreate()
+        alarmScheduler = AlarmSchedulerImpl(this)
+    }
+}

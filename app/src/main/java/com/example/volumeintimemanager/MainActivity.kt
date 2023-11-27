2540b9a28ce4e219e6d1d11983f9672c0192a165
Przemysław Kłos <pklos1992@gmail.com>
2023-11-27 14:56:38 +0100

    AlarmSchedulerImpl provided to HomeView

--- app/src/main/java/com/example/volumeintimemanager/MainActivity.kt
+++ app/src/main/java/com/example/volumeintimemanager/MainActivity.kt
@@ -15,9 +15,11 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val alarmsScheduler = (application as VolumeInTimeManager).alarmScheduler
+
         setContent {
             Surface {
-                Home()
+                Home(alarmScheduler = alarmsScheduler)
             }
         }
 

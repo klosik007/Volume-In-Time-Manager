9a6c6624148321cfc7bdb45691cd10cf39db1891
Przemysław Kłos <pklos1992@gmail.com>
2023-11-27 14:46:17 +0100

    NotificationPolicyAccess permission

--- app/src/main/java/com/example/volumeintimemanager/MainActivity.kt
+++ app/src/main/java/com/example/volumeintimemanager/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.example.volumeintimemanager
 
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,6 +19,12 @@ class MainActivity : ComponentActivity() {
             Surface {
                 Home()
             }
+        }
+
+        val notificationManager = baseContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (!notificationManager.isNotificationPolicyAccessGranted) {
+            val intent = Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
+            startActivity(intent)
         }
     }
 }
